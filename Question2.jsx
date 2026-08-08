7bd2aaaf5708faf8cb38b2bd520a7a7f1fd63725
Poornika R <poornika0406@gmail.com>
2026-08-08 18:48:05 +0530

    practice

---
+++ Question2.jsx
@@ -0,0 +1,30 @@
+function Skills(){
+    return(
+        <h1>My skills</h1>
+    );
+}
+
+function List(){
+    return(
+        
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Git</li>
+            <li>GitHub</li>
+        </ul>
+        
+    );
+}
+
+function Question2(){
+    return(
+        <div>
+        <Skills />
+        <List />
+        </div>
+    );
+}
+export default Question2;
