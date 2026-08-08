3909d2d437056d840232adbc4361383d8497ff5d
Poornika R <poornika0406@gmail.com>
2026-08-08 17:57:12 +0530

    practice

---
+++ Question1.jsx
@@ -0,0 +1,23 @@
+function Greeting(){
+    return(
+    <h1>Hello React!</h1>
+    );
+}
+function Reac(){
+    return(
+   <p>React is js library</p>
+    );
+}
+
+function Question1(){
+    return(
+        <div>
+        <Greeting/>
+        <Reac/>
+
+        </div>
+
+    );
+}
+
+export default Question1;
